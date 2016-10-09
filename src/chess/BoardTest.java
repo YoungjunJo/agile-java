f1452f95ed7476cf2f4737f2cad584f9bdf86138
YoungjunJo <creativestep.jyj@gmail.com>
2016-10-10 00:25:07 +0900

    ch02-10 #18 setUp()메소드 사용

--- src/chess/BoardTest.java
+++ src/chess/BoardTest.java
@@ -6,22 +6,24 @@ import static pieces.Pawn.BLACK;
 import static pieces.Pawn.WHITE;
 
 public class BoardTest extends junit.framework.TestCase{
-	private Board board = new Board();	
+	private Board board;
+	
+	public void setUp(){
+		board = new Board();
+	}
+	
 	public void testCreate(){
 		Pawn firstPawn = new Pawn();
 		board.addPawn(firstPawn);
 		assertEquals(WHITE, firstPawn.getColor());
 		assertEquals(1, board.getNumberOfPawns());
 		assertEquals(firstPawn, board.get(0));
-		//보드에 놓여져 있는 말의 목록 1번 확인 
-		System.out.println(board.get(0));
 		
 		Pawn secondPawn = new Pawn(BLACK);
 		board.addPawn(secondPawn);
 		assertEquals(BLACK, secondPawn.getColor());
 		assertEquals(2, board.getNumberOfPawns());
-		//보드에 놓여져 있는 말의 목록 2번 확인 
-		System.out.println(board.get(1));	
+		assertEquals(secondPawn, board.get(1));	
 		
 	}
 }
