5689ac2f332fcbb3867ee02fb9d48c9f786eb748
YoungjunJo <creativestep.jyj@gmail.com>
2016-10-09 22:38:55 +0900

    ch02-03 #13 1) Board 클래스 정의 2) 말이 없는 상태로 시작(TDD)

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -4,23 +4,17 @@ import java.util.ArrayList;
 import pieces.Pawn;
 
 public class Board{
-		//String [][]chessBoard= new String[8][8]; 
-	
 	private ArrayList<Pawn> pawns = new ArrayList<Pawn>();
-	
-	
 	public ArrayList<Pawn> getAllPawn(){
 		return pawns;
 	}
-	
+
 	public void addPawn(Pawn pawn){
-		pawns.add(pawn);
-		
+		pawns.add(pawn);		
 	}
-	
+
 	int getNumberOfPawns(){
 		return pawns.size();
 	}
-	
 	
 }
--- src/chess/BoardTest.java
+++ src/chess/BoardTest.java
@@ -1,25 +1,24 @@
 package chess;
 
-import java.util.ArrayList;
 import junit.framework.TestCase;
 import pieces.Pawn;
 import static pieces.Pawn.BLACK;
 import static pieces.Pawn.WHITE;
 
-
 public class BoardTest extends junit.framework.TestCase{
-	private Board board;
-	
+	private Board board = new Board();	
 	public void testCreate(){
 		Pawn firstPawn = new Pawn();
 		assertEquals(WHITE, firstPawn.getColor());
 		Pawn secondPawn = new Pawn(BLACK);
 		assertEquals(BLACK, secondPawn.getColor());
 		
+		//체스판에 말이 없는 상태 확인 
+		assertEquals(0, board.getNumberOfPawns());
+		
 		board.addPawn(firstPawn);
 		board.addPawn(secondPawn);
 		assertEquals(2, board.getNumberOfPawns());
 		
 	}
-
 }
