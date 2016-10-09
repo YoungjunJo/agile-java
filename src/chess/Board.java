f3addf7991b00a7183d3bf6fce34e368002fbcf3
YoungjunJo <creativestep.jyj@gmail.com>
2016-10-09 23:42:58 +0900

    ch02-05 #15 javadoc작성

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -3,20 +3,41 @@ package chess;
 import java.util.ArrayList;
 import pieces.Pawn;
 
+/**
+ * Provides a representation of a Chess Board
+ * @author JYJ
+ */
 public class Board{
 	private ArrayList<Pawn> pawns = new ArrayList<Pawn>();
+
+	/**
+	 * Getting the pawns list
+	 * @return pawns the ArrayList
+	 */
 	public ArrayList<Pawn> getAllPawn(){
 		return pawns;
 	}
 
+	/**
+	 * Adding a Pawn on the ArrayList
+	 * @param pawn the Pawn will be added at the ArrayList
+	 */
 	public void addPawn(Pawn pawn){
 		pawns.add(pawn);		
 	}
 
+	/**
+	 * Getting a number of pawns
+	 * @return Returns the number of pawns in this list.
+	 */
 	int getNumberOfPawns(){
 		return pawns.size();
 	}
 	
+	/**
+	 * Getting a Pawn's position
+	 * @return Returns the pawn at the specified position in this list.
+	 */
 	Pawn get(int index){
 		return pawns.get(index);
 	}
--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,23 +1,43 @@
 package pieces;
 
+/**
+ * Provides a representation of a Pawn
+ * @author JYJ
+ */
+
 public class Pawn{
 	public static final String WHITE = "white";
 	public static final String BLACK = "black";
 	
 	String color;
 	
+	/**
+	 * Constructs a Pawn and set the color WHITE
+	 */
 	public Pawn(){
 		color=WHITE;
 		}
-
+	
+	/**
+	 * Constructs a Pawn having a specific color
+	 * @param color the Pawn has
+	 */
 	public Pawn(String color){
 		this.color=color;
 	}
 	
+	/**
+	 * Getting a pawn's color 
+	 * @return color the Pawn has
+	 */
 	public String getColor(){
 		return color;
 	}
 
+	/**
+	 * Setting a pawn's color
+	 * @param color the Pawn has
+	 */
 	public void setColor(String color){
 		this.color = color;
 	}
